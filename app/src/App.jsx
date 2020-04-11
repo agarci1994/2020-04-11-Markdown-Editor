aad80450d874953841a7a9c694638a16b258e1c8
Alejandro García <alejandro.garci.rodr@gmail.com>
2020-04-11 20:04:01 +0200

    finish

---
+++ app/src/App.jsx
@@ -0,0 +1,37 @@
+import React, { useState } from "react";
+import "./App.css";
+import { Controlled as CodeMirror } from "react-codemirror2";
+import marked from "marked";
+
+import "codemirror/lib/codemirror.css";
+import "codemirror/mode/markdown/markdown";
+
+function App() {
+  const [value, setValue] = useState("// You can write the code");
+  const options = {
+    viewportMargin: Infinity,
+    mode: "markdown",
+    lineNumbers: true,
+    theme: "default height500 readonly"
+  };
+
+  return (
+    <>
+      <h1 className="margin">Markdown Editor</h1>
+      <div className="App">
+        <CodeMirror
+          value={value}
+          options={options}
+          onBeforeChange={(editor, data, value) => setValue(value)}
+          className="margin"
+        />
+        <div
+          dangerouslySetInnerHTML={{ __html: marked(value) }}
+          className="margin"
+        ></div>
+      </div>
+    </>
+  );
+}
+
+export default App;
