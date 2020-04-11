8666351fc1a4ee8e84ee255a6042b16262e53c08
Alejandro García <alejandro.garci.rodr@gmail.com>
2020-04-11 20:51:56 +0200

    electron

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,20 +1,21 @@
 import React, { useState } from "react";
-import "./App.css";
 import { Controlled as CodeMirror } from "react-codemirror2";
 import marked from "marked";
 
+import "./App.css";
 import "codemirror/lib/codemirror.css";
 import "codemirror/mode/markdown/markdown";
 
-function App() {
-  const [value, setValue] = useState("// You can write the code");
-  const options = {
-    viewportMargin: Infinity,
-    mode: "markdown",
-    lineNumbers: true,
-    theme: "default height500 readonly"
-  };
 
+  function App() {
+    const [value, setValue] = useState("// You can write the code");
+    const options = {
+      viewportMargin: Infinity,
+      mode: "markdown",
+      lineNumbers: true,
+      theme: "default height500 readonly"
+    };
+    
   return (
     <>
       <h1 className="margin">Markdown Editor</h1>
